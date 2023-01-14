f56cb40f40036060b9b629597d1b3f7389bfe9f2
fady <gamilfady605@gmail.com>
2023-01-14 23:03:49 +0200

    Configure the IdentityDbContext inside the Infrastructure project

--- Infrastructure/Data/DataContext.cs
+++ Infrastructure/Data/DataContext.cs
@@ -1,10 +1,11 @@
 using Microsoft.EntityFrameworkCore;
 using Eventsocity.Domain.Entities;
 using Eventsocity.Infrastructure.Configurations;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 
 namespace Eventsocity.Infrastructure.Data;
 
-public class DataContext : DbContext
+public class DataContext : IdentityDbContext<User>
 {
    public DataContext(DbContextOptions<DataContext> options) : base(options)
    {
@@ -12,6 +13,9 @@ public class DataContext : DbContext
    }
    protected override void OnModelCreating(ModelBuilder modelBuilder)
    {
+      // we have to call the OnModelCreating of the base class which is here the `IdentityDbContext` because there are essential methods that are called in the 
+      // IdentityDbContext's OnModelCreating method and here we overriden it 
+      base.OnModelCreating(modelBuilder);
       modelBuilder.ApplyConfiguration<Event>(new EventsConfig());
    }
    public DbSet<Event> Events {get; set;}
